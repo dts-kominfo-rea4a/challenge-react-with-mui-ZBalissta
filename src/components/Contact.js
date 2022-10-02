2c1ad287bea5150989b8442127fa2a6823183bb6
ZBalissta <salimystogan69@gmail.com>
2022-10-02 23:49:39 +0700

    add contact app

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,74 @@
 // Contact component dapat berupa MUI ListItem
 // https://mui.com/material-ui/react-list/#folder-list
 import React from 'react';
+import {
+    List,
+    ListItem,
+    ListItemText,
+    ListItemAvatar,
+    Avatar,
+    Typography
+} from '@mui/material';
 
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 // atau langsung tambahkan dengan sx={{}}
-const Contact = ({ data }) => {
+const Contact = ({data}) => {
+    let items = "";
+    if(Array.isArray(data)) {
+        items = (
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                {data.map((row) => (
+                    <ListItem key={row.id}>
+                        <ListItemAvatar>
+                        <Avatar>
+                            <img alt="" src={row.photo} />
+                        </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary={row.name}
+                            secondary={
+                                <>
+                                     <Typography variant="span">
+                                        {row.phone}
+                                     </Typography><br/>
+                                     <Typography variant="span">
+                                        {row.email}
+                                     </Typography>
+                                </>
+                            } />
+                    </ListItem>
+                ))}
+            </List>
+        );
+    } else {
+        items = (
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                <ListItem key={data.id}>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <img alt="" src={data.photo} />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary={data.name}
+                        secondary={
+                            <>
+                                <Typography variant="span">
+                                {data.phone}
+                                </Typography><br/>
+                                <Typography variant="span">
+                                {data.email}
+                                </Typography>
+                            </>
+                        } />
+                </ListItem>
+            </List>
+        );
+    }
     // Contact berisi foto, nama, telepon, dan email
-    return (<></>);
+    return (
+        <>{items}</>
+    );
 };
 
 export default Contact;
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,14 +2,96 @@
 // https://mui.com/material-ui/react-text-field/#basic-textfield
 // dan Card
 // https://mui.com/material-ui/react-card/#basic-card
+import React, { useState } from "react";
+import { TextField, Button } from "@mui/material";
+import { useSlotProps } from "@mui/base";
+
 
 const ContactForm = () => {
     // Form berisi name, phone, email, dan photo url
+    const [getName, setName] = React.useState('Jhon Doe');
+    const [getPhone, setPhone] = React.useState('089600000101');
+    const [getEmail, setEmail] = React.useState('jhondoe@gmail.com');
+    const [getPhoto, setPhoto] = React.useState('http://placekitten.com/500');
+
     // Buatlah state newContact berupa objek sesuai dengan data yang ada
+    const [newContact, setNewContact] = React.useState({
+        name: getName,
+        phone: getPhone,
+        email: getEmail,
+        photo: getPhoto
+    });
+
+    const onSubmit = (onSub) => {
+        onSub.preventDefault();
+        props.fnAddContacts({
+            name: getName,
+            phone: getPhone,
+            email: getEmail,
+            photo: getPhoto
+        })
+
+        setName ('Howdy');
+        setPhone('08961324240101');
+        setEmail('howdy@gmail.com');
+        setPhoto('http://placekitten.com/500');
+    };
+
+    const nameOnChange = (onSub) => {
+        setName(onSub.target.value);
+    };
+
+    const phoneOnChange = (onSub) => {
+        setPhone(onSub.target.value);
+    };
+
+    const emailOnChange = (onSub) => {
+        setEmail(onSub.target.value);
+    };
+
+    const photoOnChange = (onSub) => {
+        setPhoto(onSub.target.value);
+    };
 
     return (
-        <> </>
+        <> 
+            <form onSubmit={onSubmit}>
+                <TextField fullWidth
+                    value={getName}
+                    onChange={nameOnChange}
+                    id="name"
+                    label="Name "
+                    variant="filled" 
+                />
+
+                <TextField fullWidth sx={{marginTop: '0.5em'}}
+                    value={getPhone}
+                    onChange={phoneOnChange}
+                    id="phone"
+                    label="Phone "
+                    variant="filled" 
+                />
+
+                <TextField fullWidth sx={{marginTop: '0.5em'}}
+                    value={getEmail}
+                    onChange={emailOnChange}
+                    id="email"
+                    label="Email"
+                    variant="filled"
+                />
+
+                <TextField fullWidth sx={{marginTop: '0.5em'}}
+                    value={getPhoto}
+                    onChange={photoOnChange}
+                    id="photo"
+                    label="Photo"
+                    variant="filled"
+                />
+
+                <Button type="submit" sx={{marginTop: "0,5em"}} variant="outlined">Add New Contact</Button>
+            </form>
+        </>
     );
-}
+};
 
 export default ContactForm;
